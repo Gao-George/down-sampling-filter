131ccc19e2a0b1f1699b769d081ceff10ec66118
Gao-George <154294519+Gao-George@users.noreply.github.com>
2023-12-20 22:32:42 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,60 +1,37 @@
 #include <iostream>
-#include <fstream>
 #include <cmath>
-#include <vector>
+#include <fstream>
 #include "cic.h"
 using namespace std;
 
-const double PI = 3.141592653589793;
-
 int main() {
-	vector<double> xAxis, original;
+	const double PI = 3.1415926535;
 	
-	// 画x轴
-	for (int x = -250; x < 251; x++) {
-		xAxis.push_back(x);
-	}
-	// 画原函数图像
-	for (int i = 0; i < 100; i++) {
-		original.push_back(sin(i * PI / 100));
-	}
-	for (int i = 0; i < 100; i++) {
-		original.push_back(0);
-	}
-	for (int i = 0; i < 100; i++) {
-		original.push_back(sin(i * PI / 100));
-	}
-	for (int i = 0; i < 100; i++) {
-		original.push_back(0);
-	}
-	for (int i = 0; i < 100; i++) {
-		original.push_back(sin(i * PI / 100));
+	// signal with carrier
+	double carried[800];
+	for (int i = 0; i < 800; i++) {
+		carried[i] = sin(PI / 400 * i) * sin(PI / 12 * i);
 	}
 
-	// 上变频
-	vector<double> up = comb(original, 5); 
-	up = upSample(up, 3);
-	up = integration(up, 5);
-	
-	// 下变频
-	vector<double> down = integration(original, 5);
-	down = downSample(down, 3);
-	down = comb(down, 5);
-	
-	// 将数据点写进CSV
+	CIC filter(5, 20);
+	double output[40];
+	double integrated;
+	for (int i = 0; i < 800; i++) {
+		integrated = filter.integral(carried[i]);
+		if (filter.downCounter()) {
+			output[i / 20] = filter.comb(integrated);
+		}
+	}
+
+	double x[40];
+	for (int i = 0; i < 40; i++) {
+		x[i] = i;
+	}
 	ofstream plt;
 	plt.open("plot.csv");
-	plt << "x,originalSignal,upSample,downSample" << endl;
-
-	for (int i = 0; i < 500; i++) {
-		if (i >= down.size() && i < (down.size()*2-1)) {
-			plt << xAxis[i] << ',' << original[i] << ',' << up[i+500] << ',' << down[i-167] << endl;
-		}
-		else {
-			plt << xAxis[i] << ',' << original[i] << ',' << up[i + 500] << ',' << 0 << endl;
-		}
-		
+	plt << "filtered signal,x" << endl;
+	for (int i = 0; i < 40; i++) {
+		plt << output[i] << ',' << x[i] << endl;
 	}
-
 	plt.close();
 }
