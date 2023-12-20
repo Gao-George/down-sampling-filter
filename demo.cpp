9ebeac70e68cd7f57f89750b5aa00c213f28b2ed
Gao-George <154294519+Gao-George@users.noreply.github.com>
2023-12-20 23:09:15 +0800

    demo.cpp

--- demo.cpp
+++ demo.cpp
@@ -7,22 +7,24 @@ using namespace std;
 int main() {
 	const double PI = 3.1415926535;
 	
-	// signal with carrier
+	// generate a signal with carrier
 	double carried[800];
 	for (int i = 0; i < 800; i++) {
 		carried[i] = sin(PI / 400 * i) * sin(PI / 12 * i);
 	}
 
-	CIC filter(5, 20);
+	// core part of CIC filtering
+	CIC filter(5, 20);		// initiate a 5-order CIC filter, with a down sampling rate of 20
 	double output[40];
 	double integrated;
 	for (int i = 0; i < 800; i++) {
-		integrated = filter.integral(carried[i]);
-		if (filter.downCounter()) {
-			output[i / 20] = filter.comb(integrated);
+		integrated = filter.integral(carried[i]);		// upon the input of each bit, do integration
+		if (filter.downCounter()) {				// down sampling
+			output[i / 20] = filter.comb(integrated);	// after down sanpling, use comb filter
 		}
 	}
 
+	// plot the result
 	double x[40];
 	for (int i = 0; i < 40; i++) {
 		x[i] = i;
